89b6aa72a98ab767eec694b4909f0c8c17227459
Tristan Jahnke <trjahnke@protonmail.com>
2026-06-03 11:22:35 -0500

    feat: Parse resource, data, and module blocks
    Signed-off-by: Tristan Jahnke <trjahnke@protonmail.com>

---
+++ pkg/config/modules.go
@@ -0,0 +1,91 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package config
+
+import (
+	"github.com/hashicorp/hcl/v2"
+	"github.com/remoterabbit/open-inspector/pkg/model"
+	"github.com/zclconf/go-cty/cty"
+)
+
+var moduleSchema = &hcl.BodySchema{
+	Attributes: []hcl.AttributeSchema{
+		{Name: "source", Required: true},
+		{Name: "version"},
+		{Name: "count"}, {Name: "for_each"},
+		{Name: "providers"}, {Name: "depends_on"},
+	},
+}
+
+func decodeModuleCallBlock(block *hcl.Block, source []byte, module *model.Module) model.Diagnostics {
+	moduleCall := model.ModuleCall{
+		Name:  block.Labels[0],
+		Range: model.RangeFromHcl(block.DefRange),
+	}
+
+	inner, _, hdiag := block.Body.PartialContent(moduleSchema)
+	diags := model.DiagnosticsFromHCL(hdiag)
+
+	if attribute, ok := inner.Attributes["source"]; ok {
+		if str, ok := literalString(attribute.Expr); ok {
+			moduleCall.Source = str
+		}
+	}
+
+	if attribute, ok := inner.Attributes["version"]; ok {
+		if str, ok := literalString(attribute.Expr); ok {
+			moduleCall.Version = str
+		}
+	}
+
+	if attribute, ok := inner.Attributes["count"]; ok {
+		expression := capture(attribute.Expr, source)
+		moduleCall.Count = &expression
+	}
+
+	if attribute, ok := inner.Attributes["for_each"]; ok {
+		expression := capture(attribute.Expr, source)
+		moduleCall.ForEach = &expression
+	}
+
+	if attribute, ok := inner.Attributes["depends_on"]; ok {
+		deps, ddiag := decodeTraversalList(attribute.Expr)
+		diags = append(diags, ddiag...)
+		moduleCall.DependsOn = deps
+	}
+
+	if attribute, ok := inner.Attributes["providers"]; ok {
+		pMap, pdiag := decodeProviderMap(attribute.Expr)
+		diags = append(diags, pdiag...)
+		moduleCall.Providers = pMap
+	}
+
+	module.ModuleCalls = append(module.ModuleCalls, moduleCall)
+	return diags
+}
+
+// decodeProviderMap handles `providers = { aws = aws.east }`. Keys are bare
+// identifiers (decode as literal strings); values are traversals (use AbsTraversalForExpr,
+// not Value()).
+func decodeProviderMap(expression hcl.Expression) (map[string]string, model.Diagnostics) {
+	pairs, hdiag := hcl.ExprMap(expression)
+	diags := model.DiagnosticsFromHCL(hdiag)
+	output := map[string]string{}
+
+	for _, pair := range pairs {
+		key, kdiag := pair.Key.Value(nil)
+		diags = append(diags, model.DiagnosticsFromHCL(kdiag)...)
+		if key.IsNull() || key.Type() != cty.String {
+			continue
+		}
+		traversal, tdiag := hcl.AbsTraversalForExpr(pair.Value)
+		if tdiag.HasErrors() {
+			diags = append(diags, model.DiagnosticsFromHCL(tdiag)...)
+			continue
+		}
+		output[key.AsString()] = traversalString(traversal)
+	}
+	return output, diags
+}
---
+++ pkg/config/resources.go
@@ -0,0 +1,144 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package config
+
+import (
+	"github.com/hashicorp/hcl/v2"
+
+	"github.com/remoterabbit/open-inspector/pkg/model"
+)
+
+var resourceSchema = &hcl.BodySchema{
+	Attributes: []hcl.AttributeSchema{
+		{Name: "count"},
+		{Name: "for_each"},
+		{Name: "provider"},
+		{Name: "depends_on"},
+	},
+	Blocks: []hcl.BlockHeaderSchema{{Type: "lifecycle"}},
+}
+
+func decodeResourceBlock(block *hcl.Block, source []byte, mode model.ResourceMode, module *model.Module) model.Diagnostics {
+	resource := model.Resource{
+		Mode:  mode,
+		Type:  block.Labels[0],
+		Name:  block.Labels[1],
+		Range: model.RangeFromHcl(block.DefRange),
+	}
+
+	inner, _, hdiag := block.Body.PartialContent(resourceSchema)
+	diags := model.DiagnosticsFromHCL(hdiag)
+
+	if attribute, ok := inner.Attributes["count"]; ok {
+		expression := capture(attribute.Expr, source)
+		resource.Count = &expression
+	}
+
+	if attribute, ok := inner.Attributes["for_each"]; ok {
+		expression := capture(attribute.Expr, source)
+		resource.ForEach = &expression
+	}
+
+	if attribute, ok := inner.Attributes["provider"]; ok {
+		if traversal, tdiag := hcl.AbsTraversalForExpr(attribute.Expr); !tdiag.HasErrors() {
+			resource.Provider = traversalString(traversal)
+		} else {
+			diags = append(diags, model.DiagnosticsFromHCL(tdiag)...)
+		}
+	}
+
+	if attribute, ok := inner.Attributes["depends_on"]; ok {
+		deps, ddiag := decodeTraversalList(attribute.Expr)
+		diags = append(diags, ddiag...)
+		resource.DependsOn = deps
+	}
+
+	for _, lifeBlock := range inner.Blocks.OfType("lifecycle") {
+		lifeCycle, ldiag := decodeLifecycle(lifeBlock, source)
+		diags = append(diags, ldiag...)
+		resource.Lifecycle = lifeCycle
+	}
+
+	if mode == model.ManagedResourceMode {
+		module.ManagedResources = append(module.ManagedResources, resource)
+	} else {
+		module.DataResources = append(module.DataResources, resource)
+	}
+
+	return diags
+}
+
+var lifecycleSchema = &hcl.BodySchema{
+	Attributes: []hcl.AttributeSchema{
+		{Name: "create_before_destroy"},
+		{Name: "prevent_destroy"},
+		{Name: "ignore_changes"},
+		{Name: "replace_triggered_by"},
+	},
+	Blocks: []hcl.BlockHeaderSchema{
+		{Type: "precondition"},
+		{Type: "postcondition"},
+	},
+}
+
+func decodeLifecycle(block *hcl.Block, source []byte) (*model.Lifecycle, model.Diagnostics) {
+	inner, _, hdiag := block.Body.PartialContent(lifecycleSchema)
+	diags := model.DiagnosticsFromHCL(hdiag)
+	lifeCycle := &model.Lifecycle{}
+
+	if block, set, _ := decodeBool(inner.Attributes["create_before_destroy"]); set {
+		lifeCycle.CreateBeforeDestroy = &block
+	}
+
+	if block, set, _ := decodeBool(inner.Attributes["prevent_destroy"]); set {
+		lifeCycle.PreventDestroy = &block
+	}
+
+	if attribute, ok := inner.Attributes["ignore_changes"]; ok {
+		// ignore_changes accepts either a list (`[id, tags]`) OR the bare keyword
+		// `all`. AbsTraversalForExpr handles `all` as a single TraverseRoot; fall
+		// back to list decoding otherwise.
+		if traversal, tdiag := hcl.AbsTraversalForExpr(attribute.Expr); !tdiag.HasErrors() {
+			lifeCycle.IgnoreChanges = []string{traversalString(traversal)}
+		} else {
+			list, ldiag := decodeTraversalList(attribute.Expr)
+			diags = append(diags, ldiag...)
+			lifeCycle.IgnoreChanges = list
+		}
+	}
+
+	if attribute, ok := inner.Attributes["replace_triggered_by"]; ok {
+		list, ldiag := decodeTraversalList(attribute.Expr)
+		diags = append(diags, ldiag...)
+		lifeCycle.ReplaceTriggeredBy = list
+	}
+
+	for _, preBlock := range inner.Blocks.OfType("precondition") {
+		value, vdiag := decodeConditionBlock(preBlock, source)
+		diags = append(diags, vdiag...)
+		lifeCycle.Preconditions = append(lifeCycle.Preconditions, value)
+	}
+
+	for _, postBlock := range inner.Blocks.OfType("postcondition") {
+		value, vdiag := decodeConditionBlock(postBlock, source)
+		diags = append(diags, vdiag...)
+		lifeCycle.Postconditions = append(lifeCycle.Postconditions, value)
+	}
+
+	return lifeCycle, diags
+}
+
+// decodeConditionBlock is the same shape as the variable validation block.
+// Pull it out so variables.go and resources.go share one implementation.
+func decodeConditionBlock(block *hcl.Block, source []byte) (model.Validation, model.Diagnostics) {
+	inner, _, hdiag := block.Body.PartialContent(validationSchema)
+	diags := model.DiagnosticsFromHCL(hdiag)
+
+	return model.Validation{
+		Condition:    capture(inner.Attributes["condition"].Expr, source),
+		ErrorMessage: capture(inner.Attributes["error_message"].Expr, source),
+		Range:        model.RangeFromHcl(block.DefRange),
+	}, diags
+}
